4fa994df15614416c771e7e83b95b153fba0d3c5
laszej <88169478+laszej@users.noreply.github.com>
2021-11-13 18:15:36 +0100

    Add files via upload

---
+++ index0.js
@@ -0,0 +1,34 @@
+let addTask = document.getElementById("dodaj");
+let removeAll = document.getElementById("usun");
+let noweZadanie = document.getElementById("zadania")
+let lista = document.getElementById('lista');
+noweZadanie.value = "";
+
+
+
+addTask.addEventListener("click", function() {
+    let nowySpan = document.createElement("span")
+    lista.appendChild(nowySpan);
+    
+    nowySpan.innerHTML = `<span id="wyjeb mnie">
+    ${noweZadanie.value} <button id="remove">Remove </button> <br><br></span>`;
+    nowySpan.addEventListener('click', function (){
+        lista.removeChild(nowySpan);})})
+
+        removeAll.addEventListener("click",  function() {lista.innerHTML=""})
+
+    
+
+
+    
+
+
+        
+       
+
+
+
+
+
+
+
